3abe4b6ddea8d55e44993d85659ff98813372069
Ubuntu <ubuntu@ip-172-31-35-40.ec2.internal>
2019-10-24 22:42:55 +0000

    update_controller_spec

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -60,5 +60,17 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+  
+  def search_by_director
+    @movie = Movie.find(params[:id])
+    director = @movie.director
+    if director.nil? then
+      flash[:notice] = "'#{@movie.title}' has no director info"
+      redirect_to movies_path
+    else
+      @movies = Movie.where(Director: director)
+    end
+  end
+  
 
 end
